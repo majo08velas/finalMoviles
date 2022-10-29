8aa83cab2f3ea4a6592ac439d6a880f76e0fbe8d
David Avila <adavila59@ucatolica.edu.co>
2022-10-29 18:50:14 -0500

    Clase actores, prueba con 2 actores, dialog INFO

---
+++ app/src/main/java/com/example/myapplication/Actor.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication
+
+import androidx.appcompat.widget.AppCompatImageView
+
+data class Actor(val name: String, val age: Int, val image:Int)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonUno: Button = findViewById(R.id.button)
+        val buttonDos: Button = findViewById(R.id.button2)
+        val buttonDesarrolladores: Button = findViewById(R.id.button3)
+
+        val imageView: ImageView = findViewById(R.id.imageView)
+
+        val text: TextView = findViewById(R.id.cambiese)
+
+        val actor1 = Actor("Matt", 12, R.drawable.matt)
+        val actor2 = Actor("Jessica", 43,R.drawable.jessica )
+
+        buttonUno.setOnClickListener {
+            text.text = actor1.name
+            imageView.setImageResource(actor1.image)
+        }
+
+        buttonDos.setOnClickListener {
+            text.text = actor2.name
+            imageView.setImageResource(actor2.image)
+        }
+
+        buttonDesarrolladores.setOnClickListener {
+            val dialog = CustomAbout()
+            dialog.show(supportFragmentManager, "Custom About Me")
+        }
     }
 }
