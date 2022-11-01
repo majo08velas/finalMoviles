ff7faaff78a999956689eda0f22d4ae1463ff4a8
Maria Jose <mjaranguren07@ucatolica.edu.co>
2022-11-01 13:10:13 -0500

    Modificación estructura y se agregaron imágenes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,25 +3,28 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import com.example.myapplication.ActorPrefs.Companion.prefs
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById<Toolbar>(R.id.toolbar))
 
-        val buttonUno: Button = findViewById(R.id.button)
-        val buttonDos: Button = findViewById(R.id.button2)
-        val buttonProfileActor:Button = findViewById(R.id.button4)
+        //val buttonUno: Button = findViewById(R.id.button)
+        //val buttonDos: Button = findViewById(R.id.button2)
+        val buttonProfileActor:Button = findViewById(R.id.buttonProfileActor)
 
-        val buttonDesarrolladores: Button = findViewById(R.id.button3)
+        //val imageView: ImageView = findViewById(R.id.imageView)
 
-        val imageView: ImageView = findViewById(R.id.imageView)
-
-        val text: TextView = findViewById(R.id.cambiese)
+        //val text: TextView = findViewById(R.id.cambiese)
 
 
 
@@ -34,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // Valor por Defecto
         var actor:Actor = Actor("Jessica", 43,R.drawable.jessica )
 
-        buttonUno.setOnClickListener {
+        /*buttonUno.setOnClickListener {
             text.text = "${character1.name} ${character1.lastName}"
             imageView.setImageResource(actor1.image)
             actor = actor1
@@ -46,7 +49,7 @@ class MainActivity : AppCompatActivity() {
             imageView.setImageResource(actor2.image)
             actor = actor2
 
-        }
+        }*/
 
         buttonProfileActor.setOnClickListener {
             val i = Intent(this, ProfileActor::class.java)
@@ -55,12 +58,29 @@ class MainActivity : AppCompatActivity() {
             startActivity(i)
         }
 
-        buttonDesarrolladores.setOnClickListener {
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_resource,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when(item.itemId) {
+        R.id.home -> {
+            Toast.makeText(this@MainActivity, "Inicio", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            true
+        }
+        R.id.info -> {
+            Toast.makeText(this@MainActivity, "Info Desarrolladores", Toast.LENGTH_SHORT).show()
             val dialog = CustomAbout()
             dialog.show(supportFragmentManager, "Custom About Me")
+            true
         }
-
+        else -> super.onOptionsItemSelected(item)
     }
+
 }
 
 
